bbbdef76424cdfbf641fbd2c2006590c1f511010
Aditya Kanteti <100003303+IJustWantAJob@users.noreply.github.com>
2025-04-23 14:31:28 -0700

    test

--- ps2-starter/analyze.c
+++ ps2-starter/analyze.c
@@ -1,14 +1,36 @@
 #include "analyze.h"
 #include <string.h>
-#include <assert.h>
+#include <stdlib.h>
 
-void analyzeURL(char* url,char* host,int* port,char* doc)
+void analyzeURL(char* url, char* host, int* port, char* doc)
 {
-   /* TODO Q1: Parse the URL - Implement the URL decomposition.
-    * input: URL the given url
-      output: host : where you place the host part of the URL
-              port : where you place the port number (if any)
-              doc  : where you place the document name
-    * hint: scan the url for special character such as : and /. These characters mark the beginning and end of the above variables.
-   */
+    char* p = url;
+
+    // skip hhtp:// if there
+    if (strncmp(p, "http://", 7) == 0)
+        p += 7;
+
+    // extract host
+    char* hstart = p;
+    while (*p && *p != ':' && *p != '/')
+        p++;
+    int hlen = p - hstart;
+    strncpy(host, hstart, hlen);
+    host[hlen] = '\0';
+
+    // default to 80, override if :<num> is there
+    *port = 80;
+    if (*p == ':') {
+        p++;
+        *port = atoi(p);
+        while (*p && *p != '/')
+            p++;
+    }
+
+    // Rest is document path, include leading slash
+    if (*p == '\0') {
+        strcpy(doc, "/");
+    } else {
+        strcpy(doc, p);
+    }
 }
